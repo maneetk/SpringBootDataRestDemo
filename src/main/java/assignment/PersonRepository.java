99e3ab5a20e1526ba3583046e957da06c835548a
maneetk <maneetk1989@gmail.com>
2015-07-09 19:12:34 +0530

    Removed controller and using Spring default Data Rest API

--- src/main/java/assignment/PersonRepository.java
+++ src/main/java/assignment/PersonRepository.java
@@ -1,10 +1,10 @@
 package assignment;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 //import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 //@RepositoryRestResource(collectionResourceRel = "people", path = "people")
-public interface PersonRepository extends CrudRepository<Person, Long> {
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 	Person findByFirstName(@Param("name") String firstName);
 }
