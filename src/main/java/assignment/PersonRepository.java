592f1fdc5abeb33595402077595dc6982c11a620
maneetk <maneetk1989@gmail.com>
2015-07-09 19:18:36 +0530

    changing the search method to return list of objects

--- src/main/java/assignment/PersonRepository.java
+++ src/main/java/assignment/PersonRepository.java
@@ -1,10 +1,12 @@
 package assignment;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 //import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 //@RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
-	Person findByFirstName(@Param("name") String firstName);
+	List<Person> findByFirstName(@Param("name") String firstName);
 }
